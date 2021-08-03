3800bd84827a574a19da04e2007ed4515dd4dc52
umbrellatun <sataporn.chn@gmail.com>
2021-08-03 11:22:59 +0700

    Update GodSplitApproveController.php

--- app/Http/Controllers/GodSplitApproveController.php
+++ app/Http/Controllers/GodSplitApproveController.php
@@ -32,26 +32,16 @@ class GodSplitApproveController extends Controller
                ->orderBy('DocuNO', 'desc')
                ->get();
           } else {
-               if(!isset($request->daterange)){
-                    $today = date("Y-m-d");
-                    $previous_day = date('Y-m-d',strtotime($today . "-15 days"));
-                    $data["daterange"] = "";
-                    $data["headers"] = ICGodSplitHD::where('AppvStatus', '=', 'Y')
-                    ->orderByRaw("ISNULL(AppvSplitStatus, ''), AppvStatus ASC")
-                    ->orderBy('DocuNO', 'desc')
-                    ->get();
-               } else {
-                    $today = date("Y-m-d");
-                    $previous_day = date('Y-m-d',strtotime($today . "-15 days"));
-                    $data["daterange"] = date_format(date_create($previous_day), "m/d/Y") . ' - ' . date_format(date_create($today), "m/d/Y");
-                    $data["headers"] = ICGodSplitHD::where('AppvStatus', '=', 'Y')
-                    ->where(function($q)use($previous_day, $today){
-                         $q->whereRaw("CONVERT(Varchar, [DocuDate], 112) BETWEEN '".$previous_day."' AND '".$today."'");
-                    })
-                    ->orderByRaw("ISNULL(AppvSplitStatus, ''), AppvStatus ASC")
-                    ->orderBy('DocuNO', 'desc')
-                    ->get();
-               }
+               $today = date("Y-m-d");
+               $previous_day = date('Y-m-d',strtotime($today . "-15 days"));
+               $data["daterange"] = date_format(date_create($previous_day), "m/d/Y") . ' - ' . date_format(date_create($today), "m/d/Y");
+               $data["headers"] = ICGodSplitHD::where('AppvStatus', '=', 'Y')
+               ->where(function($q)use($previous_day, $today){
+                    $q->whereRaw("CONVERT(Varchar, [DocuDate], 112) BETWEEN '".$previous_day."' AND '".$today."'");
+               })
+               ->orderByRaw("ISNULL(AppvSplitStatus, ''), AppvStatus ASC")
+               ->orderBy('DocuNO', 'desc')
+               ->get();
 
                // dd($data["headers"]);
           }
