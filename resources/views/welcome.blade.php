62fcea8ecd2e3a381873579b99923898dc51fa49
umbrellatun <sataporn.chn@gmail.com>
2023-03-04 11:11:22 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1135,14 +1135,14 @@
                                                            find_string = (data.ContainerNO).includes("@");
                                                            if (find_string == false){
                                                                 // console.log(getMonthNum($("#tb2_shipdate2").text()));
-                                                                if (data.Flag_st.length == 0 && (getMonthNum($("#tb2_shipdate2").text()) == getMonthNum(data.ShipDate))){
+                                                                if (data.Flag_st.length == 0 && (parseInt(getMonthNum($("#tb2_shipdate2").text())) == parseInt(getMonthNum(data.ShipDate)))) {
                                                                      chkbox = '<input type="checkbox" id="product_share_chk_'+data.RefSOCOID+'" class="form-check-input product_share_chk product_share_chk_'+data.RefSOCOID+'_'+data.ContainerNO+'_'+data.RefListNo+'" data-value="'+data.RefSOCOID+'" data-container="'+data.ContainerNO+'" data-reflistno="'+data.RefListNo+'" value="'+data.RefSOCOID+'">';
                                                                 } else {
                                                                      chkbox = '';
                                                                 }
                                                                 tr += '<tr>';
                                                                 tr += '<td>';
-                                                                tr += getMonthNum($("#tb2_shipdate").val()) + '|' + getMonthNum(data.ShipDate);
+                                                                tr += parseInt(getMonthNum($("#tb2_shipdate").val())) + '|' + parseInt(getMonthNum(data.ShipDate));
                                                                 tr += '</td>';
                                                                 tr += '<td>';
                                                                 tr += chkbox;
